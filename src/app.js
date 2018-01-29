ebda264f71357ec60a1a4c737be07dba28210e32
Dusan Zivkovic <dusan.zivkovic@cadev.rs>
2018-01-29 13:05:36 +0100

    exact true, BrowserRouter with one thing inside like div full of Route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
-ReactDOM.render(<p>this is my boilerplate</p>,document.getElementById('app'));
+const ExpenseDashboardpage = () => (
+    <div>
+        This is from dash comp
+    </div>
+);
+
+
+const expensestuff = () => (
+    <div>
+        This is f
+    </div>
+);
+
+
+const help = () => (
+    <div>
+    Help page
+    </div>
+);
+
+
+const edit = () => (
+    <div>
+    edit page
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path='/' component={ExpenseDashboardpage} exact={true}/>
+            <Route path='/create' component={expensestuff} />
+            <Route path='/edit' component={edit} />
+            <Route path='/help' component={help} />
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
