322476116a792628cefd39d2afb1b4827b8b513a
Dusan Zivkovic <dusan.zivkovic@cadev.rs>
2018-01-30 09:54:21 +0100

    react-router passes props down to components inside routes, location, match history

--- src/app.js
+++ src/app.js
@@ -1,66 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { NavLink, Link, BrowserRouter, Switch, Route } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 
 import './styles/styles.scss';
 import 'normalize.css/normalize.css';
 
-const ExpenseDashboardpage = () => (
-    <div>
-        This is from dash comp
-    </div>
-);
-
-
-const CreateExpensePage = () => (
-    <div>
-        Create expense
-    </div>
-);
-
-
-const HelpPage = () => (
-    <div>
-        Help page
-    </div>
-);
-
-
-const NotFoundPage = () => (
-    <div>
-        Error 404: Not found page - <Link to='/'>Go home</Link>
-    </div>
-);
-
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink to='/' activeClassName='is-active' exact={true}>Home</NavLink>
-        <NavLink to='/create' activeClassName='is-active'>Create</NavLink>
-        <NavLink to='/edit' activeClassName='is-active'>Edit</NavLink>
-        <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
-    </header>
-);
-
-const EditPage = () => (
-    <div>
-        edit page
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Header />
-            <Switch>
-                <Route path='/' component={ExpenseDashboardpage} exact={true} />
-                <Route path='/create' component={CreateExpensePage} />
-                <Route path='/edit' component={EditPage} />
-                <Route path='/help' component={HelpPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
-        </div>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExpenseDashboardpage = () => (
+    <div>
+        This is from dash comp
+    </div>
+);
+
+export default ExpenseDashboardpage;
