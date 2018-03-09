0d622abff744c2a5f09142c8cf3c379b7324d917
Dusan Zivkovic <dusan.zivkovic@cadev.rs>
2018-03-09 08:22:31 +0100

    edit & remove

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
 // Export a stateless functional component
 // description, amount, createdAt
 
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
-        <h1>{description}</h1>
+        <Link to={`/edit/${id}`} >
+            <h1>{description}</h1>
+        </Link>
         <p>{amount} - {createdAt}</p>
-        <button onClick={(e) => {
-            dispatch(removeExpense({ id }))
-        }}>Remove</button>
+
     </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
